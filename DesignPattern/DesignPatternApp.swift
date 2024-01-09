a2b150b560b1354732ee15e74e0dc69b1853cbcd
Camelia <cameliabraghes@gmail.com>
2024-01-09 12:43:50 +0200

    Initial Setup

---
+++ DesignPattern/DesignPatternApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignPatternApp.swift
+//  DesignPattern
+//
+//  Created by Camelia Braghes on 09.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct DesignPatternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
