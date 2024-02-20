d5e79cca9e2c202b62b87e75cb0f2d031d238929
Camelia <cameliabraghes@gmail.com>
2024-02-20 12:14:02 +0200

    Add function to show question

--- DesignPattern/MVC/RabbleWabble_final/RabbleWabble_final/Controllers/QuestionViewController.swift
+++ DesignPattern/MVC/RabbleWabble_final/RabbleWabble_final/Controllers/QuestionViewController.swift
@@ -2,20 +2,32 @@ import UIKit
 
 public class QuestionViewController: UIViewController {
     
-    public override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-    
-    
-    // MARK: - Instance Properties
     public var questionGroup = QuestionGroup.basicPhrases()
     public var questionIndex = 0
+    
     public var correctCount = 0
     public var incorrectCount = 0
+    
     public var questionView: QuestionView! {
-        guard isViewLoaded else { return nil }
-        return (view as! QuestionView)
+      guard isViewLoaded else { return nil }
+      return (view as! QuestionView)
+    }
+    
+    // MARK: - View Lifecycle
+    public override func viewDidLoad() {
+      super.viewDidLoad()
+      showQuestion()
+    }
+    
+    private func showQuestion() {
+      let question = questionGroup.questions[questionIndex]
+      
+      questionView.answerLabel.text = question.answer
+      questionView.promptLabel.text = question.prompt
+      questionView.hintLabel.text = question.hint
+      
+      questionView.answerLabel.isHidden = true
+      questionView.hintLabel.isHidden = true
     }
 }
 
