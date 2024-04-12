470ea1fa4742f68e6c50833cc69979c88c8f0076
Camelia <cameliabraghes@gmail.com>
2024-04-12 17:34:34 +0300

    Replace showQuestion function

--- DesignPattern/StrategyPattern/starter/RabbleWabble/RabbleWabble/Controllers/QuestionViewController.swift
+++ DesignPattern/StrategyPattern/starter/RabbleWabble/RabbleWabble/Controllers/QuestionViewController.swift
@@ -28,114 +28,119 @@
 
 import UIKit
 
-public protocol QuestionViewControllerDelegate: class {
-  
-  // 1
-  func questionViewController(
-    _ viewController: QuestionViewController,
-    didCancel questionGroup: QuestionGroup,
-    at questionIndex: Int)
-  
-  // 2
-  func questionViewController(
-    _ viewController: QuestionViewController,
-    didComplete questionGroup: QuestionGroup)
+public protocol QuestionViewControllerDelegate: AnyObject {
+    
+    // 1
+    func questionViewController(
+        _ viewController: QuestionViewController,
+        didCancel questionGroup: QuestionGroup,
+        at questionIndex: Int)
+    
+    // 2
+    func questionViewController(
+        _ viewController: QuestionViewController,
+        didComplete questionGroup: QuestionGroup)
 }
 
 public class QuestionViewController: UIViewController {
+    
+    // MARK: - Instance Properties
+    public weak var delegate: QuestionViewControllerDelegate?
+    
+    public var questionGroup: QuestionGroup! {
+        didSet {
+            navigationItem.title = questionGroup.title
+        }
+    }
+    
+    public var questionStrategy: QuestionStrategyPattern! {
+        didSet {
+            navigationItem.title = questionStrategy.title
+        }
+    }
+    public var questionIndex = 0
+    
+    public var correctCount = 0
+    public var incorrectCount = 0
+    
+    public var questionView: QuestionView! {
+        guard isViewLoaded else { return nil }
+        return (view as! QuestionView)
+    }
+    
+    private lazy var questionIndexItem: UIBarButtonItem = {
+        let item = UIBarButtonItem(title: "",
+                                   style: .plain,
+                                   target: nil,
+                                   action: nil)
+        item.tintColor = .black
+        navigationItem.rightBarButtonItem = item
+        return item
+    }()
+    
+    // MARK: - View Lifecycle
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        setupCancelButton()
+        showQuestion()
+    }
+    
+    private func setupCancelButton() {
+        let action = #selector(handleCancelPressed(sender:))
+        let image = UIImage(named: "ic_menu")
+        navigationItem.leftBarButtonItem =
+        UIBarButtonItem(image: image,
+                        landscapeImagePhone: nil,
+                        style: .plain,
+                        target: self,
+                        action: action)
+    }
+    
+    @objc private func handleCancelPressed(sender: UIBarButtonItem) {
+        delegate?.questionViewController(
+            self,
+            didCancel: questionGroup,
+            at: questionIndex)
+    }
+    
+    private func showQuestion() {
+        let question = questionStrategy.currentQuestion()
+        
+        questionView.answerLabel.text = question.answer
+        questionView.promptLabel.text = question.prompt
+        questionView.hintLabel.text = question.hint
+        questionView.answerLabel.isHidden = true
+        questionView.hintLabel.isHidden = true
 
-  // MARK: - Instance Properties
-  public weak var delegate: QuestionViewControllerDelegate?
-  
-  public var questionGroup: QuestionGroup! {
-    didSet {
-      navigationItem.title = questionGroup.title
+        questionIndexItem.title = questionStrategy.questionIndexTitle()
     }
-  }
-  public var questionIndex = 0
-  
-  public var correctCount = 0
-  public var incorrectCount = 0
-  
-  public var questionView: QuestionView! {
-    guard isViewLoaded else { return nil }
-    return (view as! QuestionView)
-  }
-  
-  private lazy var questionIndexItem: UIBarButtonItem = {
-    let item = UIBarButtonItem(title: "",
-                               style: .plain,
-                               target: nil,
-                               action: nil)
-    item.tintColor = .black
-    navigationItem.rightBarButtonItem = item
-    return item
-  }()
-  
-  // MARK: - View Lifecycle
-  public override func viewDidLoad() {
-    super.viewDidLoad()
-    setupCancelButton()
-    showQuestion()
-  }
-  
-  private func setupCancelButton() {
-    let action = #selector(handleCancelPressed(sender:))
-    let image = UIImage(named: "ic_menu")
-    navigationItem.leftBarButtonItem =
-      UIBarButtonItem(image: image,
-                      landscapeImagePhone: nil,
-                      style: .plain,
-                      target: self,
-                      action: action)
-  }
-  
-  @objc private func handleCancelPressed(sender: UIBarButtonItem) {
-    delegate?.questionViewController(
-      self,
-      didCancel: questionGroup,
-      at: questionIndex)
-  }
-  
-  private func showQuestion() {
-    let question = questionGroup.questions[questionIndex]
     
-    questionView.answerLabel.text = question.answer
-    questionView.promptLabel.text = question.prompt
-    questionView.hintLabel.text = question.hint
-    
-    questionView.answerLabel.isHidden = true
-    questionView.hintLabel.isHidden = true
-    questionIndexItem.title = "\(questionIndex + 1)/" +
-    "\(questionGroup.questions.count)"
-  }
-  
-  // MARK: - Actions
-  @IBAction func toggleAnswerLabels(_ sender: Any) {
-    questionView.answerLabel.isHidden =
-      !questionView.answerLabel.isHidden
-    questionView.hintLabel.isHidden =
-      !questionView.hintLabel.isHidden
-  }
-  
-  @IBAction func handleCorrect(_ sender: Any) {
-    correctCount += 1
-    questionView.correctCountLabel.text = "\(correctCount)"
-    showNextQuestion()
-  }
-  
-  @IBAction func handleIncorrect(_ sender: Any) {
-    incorrectCount += 1
-    questionView.incorrectCountLabel.text = "\(incorrectCount)"
-    showNextQuestion()
-  }
-  
-  private func showNextQuestion() {
-    questionIndex += 1
-    guard questionIndex < questionGroup.questions.count else {
-      delegate?.questionViewController(self, didComplete: questionGroup)
-      return
+    // MARK: - Actions
+    @IBAction func toggleAnswerLabels(_ sender: Any) {
+        questionView.answerLabel.isHidden =
+        !questionView.answerLabel.isHidden
+        questionView.hintLabel.isHidden =
+        !questionView.hintLabel.isHidden
     }
-    showQuestion()
-  }
+    
+    @IBAction func handleCorrect(_ sender: Any) {
+        correctCount += 1
+        questionView.correctCountLabel.text = "\(correctCount)"
+        showNextQuestion()
+    }
+    
+    @IBAction func handleIncorrect(_ sender: Any) {
+        incorrectCount += 1
+        questionView.incorrectCountLabel.text = "\(incorrectCount)"
+        showNextQuestion()
+    }
+    
+    private func showNextQuestion() {
+        questionIndex += 1
+        guard questionIndex < questionGroup.questions.count else {
+            delegate?.questionViewController(self, didComplete: questionGroup)
+            return
+        }
+        showQuestion()
+    }
 }
