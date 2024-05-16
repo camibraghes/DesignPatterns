1181990f833a4094728778f33fd7208e88764c49
Camelia <cameliabraghes@gmail.com>
2024-05-16 22:34:49 +0300

    Add playground example

--- DesignPattern/SingletonPattern/starter/FundamentalDesignPatterns.xcworkspace/FundamentalDesignPatterns.playground/Pages/Singleton.xcplaygroundpage/Contents.swift
+++ DesignPattern/SingletonPattern/starter/FundamentalDesignPatterns.xcworkspace/FundamentalDesignPatterns.playground/Pages/Singleton.xcplaygroundpage/Contents.swift
@@ -11,3 +11,34 @@
  
  ## Code Example
  */
+import UIKit
+
+// MARK: - Singleton
+let app = UIApplication.shared
+
+public class MySingleton {
+    //1
+    static let shared = MySingleton()
+    //2
+    private init() { }
+}
+//3
+let mySingleton = MySingleton.shared
+//4
+//let mySingleton1 = MySingleton()  -> 'MySingleton' initializer is inaccessible due to 'private' protection level
+
+// MARK: - Singleton Plus
+let defaultFileManager = FileManager.default
+let customFileManager = FileManager()
+
+public class MySingletonPlus {
+    //1
+    static let shared = MySingletonPlus()
+    //2
+    public init() { }
+}
+//3
+let mySingletonPlus = MySingletonPlus.shared
+//4
+let mySingletonPlus1 = MySingletonPlus()
+
