9619cc2004414c214d25076a1e101f0890babbb7
Camelia <cameliabraghes@gmail.com>
2024-05-16 23:29:16 +0300

    Override some function from UITableViewDataSource and UITableViewDelegate

--- DesignPattern/SingletonPattern/starter/RabbleWabble/RabbleWabble/Controllers/AppSettingsViewController.swift
+++ DesignPattern/SingletonPattern/starter/RabbleWabble/RabbleWabble/Controllers/AppSettingsViewController.swift
@@ -40,3 +40,36 @@ public class AppSettingsViewController: UITableViewController {
     tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
   }
 }
+
+// MARK: - UITableViewDataSource
+extension AppSettingsViewController {
+  
+  public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return QuestionStrategyType.allCases.count
+  }
+  
+  public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+    
+    let questionStrategyType = QuestionStrategyType.allCases[indexPath.row]
+    
+    cell.textLabel?.text = questionStrategyType.title()
+    
+    if appSettings.questionStrategyType == questionStrategyType {
+      cell.accessoryType = .checkmark
+    } else {
+      cell.accessoryType = .none
+    }
+    
+    return cell
+  }
+}
+
+// MARK: - UITableViewDelegate
+extension AppSettingsViewController {
+  public override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    let questionStrategyType = QuestionStrategyType.allCases[indexPath.row]
+    appSettings.questionStrategyType = questionStrategyType
+    tableView.reloadData()
+  }
+}
